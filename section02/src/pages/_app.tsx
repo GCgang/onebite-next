67888c77a559e60b45656b37efcefc30582710e2
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-12-14 16:39:51 +0900

    Feat: global-layout

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,37 +1,11 @@
+import GlobalLayout from '@/components/global-layout/global-layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push('/test');
-  };
-
-  useEffect(() => {
-    router.prefetch('/test');
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={'/'}>index</Link>
-        &nbsp;
-        <Link href={'/search'} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={'/book/1'}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 로 이동</button>
-        </div>
-      </header>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
   );
 }
