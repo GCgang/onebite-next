608e125a521f1234070020131789e942c886d8aa
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-12-14 17:44:21 +0900

    Feat: SearchableLayout

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,3 +1,19 @@
+import SearchableLayout from '@/components/searchable-alyout/searchable-layout';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+
 export default function Page() {
-  return <h1>search</h1>;
+  const router = useRouter();
+
+  const queryParam = router.query.q;
+  return (
+    <div>
+      <h1>Search</h1>
+      <p>검색어 : {queryParam}</p>
+    </div>
+  );
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
