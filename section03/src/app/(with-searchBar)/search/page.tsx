35eece01127ac3429a0558fc55a586e84d3bb3b8
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-12-23 23:51:34 +0900

    Feat: data fetch

--- section03/src/app/(with-searchBar)/search/page.tsx
+++ section03/src/app/(with-searchBar)/search/page.tsx
@@ -1,14 +1,26 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item/book-item';
+import { BookData } from '@/types';
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q?: string }>;
+  searchParams: {
+    q?: string;
+  };
 }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+  );
+
+  if (!response.ok) {
+    <div>오류가 발생 했습니다...</div>;
+  }
+
+  const searchBooks: BookData[] = await response.json();
+
   return (
     <div>
-      {books.map((book) => (
+      {searchBooks.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
