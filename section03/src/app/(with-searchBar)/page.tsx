5d45dcea5331942fc299ebffa1d20e846c34408e
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-04 00:29:57 +0900

    Feat: skeleton ui

--- section03/src/app/(with-searchBar)/page.tsx
+++ section03/src/app/(with-searchBar)/page.tsx
@@ -1,3 +1,4 @@
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton/book-list-skeleton';
 import style from './page.module.css';
 import BookItem from '@/components/book-item/book-item';
 import { BookData } from '@/types';
@@ -56,13 +57,13 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중 입니다...</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <RecoBooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중 입니다...</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <AllBooks />
         </Suspense>
       </section>
