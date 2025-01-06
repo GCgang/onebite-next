7e5b9529cd025c1c40c2b783f411e18d15640d96
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-06 11:57:11 +0900

    Feat: review 삭제기능

---
+++ section03/src/components/review-item-delete-button/review-item-delete-button.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import deleteReviewAction from '@/actions/delete-review-action';
+import { useActionState, useEffect, useRef } from 'react';
+
+interface ReviewItemDeleteButtonProps {
+  reviewId: number;
+  bookId: number;
+}
+export default function ReviewItemDeleteButton({
+  reviewId,
+  bookId,
+}: ReviewItemDeleteButtonProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [state, formAction, isPending] = useActionState(
+    deleteReviewAction,
+    null
+  );
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+  return (
+    <form ref={formRef} action={formAction}>
+      <input name='reviewId' value={reviewId} hidden readOnly />
+      <input name='bookId' value={bookId} hidden readOnly />
+      {isPending ? (
+        <div>...</div>
+      ) : (
+        <div onClick={() => formRef.current?.requestSubmit()}>삭제하기</div>
+      )}
+    </form>
+  );
+}
