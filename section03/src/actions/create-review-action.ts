55c2cab657b6add58d2c6590da2cd252adf1bf9a
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-06 11:13:23 +0900

    Feat: review 재검증, useActionState

--- section03/src/actions/create-review-action.ts
+++ section03/src/actions/create-review-action.ts
@@ -1,14 +1,19 @@
 'use server';
-export default async function createReviewAction(formData: FormData) {
+
+import { delay } from '@/util/delay';
+import { revalidateTag } from 'next/cache';
+
+export default async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return { status: false, error: '리뷰 내용과 작성자를 입력해주세요.' };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -16,9 +21,18 @@ export default async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: '',
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. : ${error}`,
+    };
   }
 }
