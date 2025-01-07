b4885af100191968a474b840b4b14c7ae9693c2f
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2025-01-07 16:48:58 +0900

    Feat: image 최적화

--- section03/src/app/book/[id]/page.tsx
+++ section03/src/app/book/[id]/page.tsx
@@ -3,6 +3,7 @@ import style from './page.module.css';
 import { notFound } from 'next/navigation';
 import ReviewEditor from '@/components/review-editor/review-editor';
 import ReiviewItem from '@/components/review-item/review-item';
+import Image from 'next/image';
 
 // export const dynamicParams = false;
 export function generateStaticParams() {
@@ -30,7 +31,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <Image
+          src={coverImgUrl}
+          width={240}
+          height={300}
+          alt={`도서 ${title}의 표지 이미지`}
+        />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
